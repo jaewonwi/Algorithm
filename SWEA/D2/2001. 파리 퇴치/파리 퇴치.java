ceaab75ca380e2a7eafacf84e01f971a028149ec
jaewonwi <80496853+jaewonwi@users.noreply.github.com>
2024-08-09 18:08:19 +0900

    [D2] Title: 파리 퇴치, Time: 119 ms, Memory: 17,432 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,51 +1,54 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int T, N, M, result;
-    static int[][] map;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine().trim());
-        StringBuilder sb = new StringBuilder();
-
-        T = Integer.parseInt(st.nextToken());
-        for (int t = 1; t <= T; t++){
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-
-            map = new int[N][N];
-            for (int i = 0; i < N; i++){
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++){
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            result = 0;
-            for (int i = 0; i < N-M+1; i++){
-                for (int j = 0; j < N-M+1; j++){
-                    hit(i, j, M);
-                }
-            }
-
-            sb.append("#").append(t).append(" ").append(result).append("\n");
-        }
-
-        System.out.println(sb);
-    }
-
-    static void hit(int y, int x, int M){
-        int sum = 0;
-        for (int i = y; i < y + M; i++){
-            for (int j = x; j < x + M; j++){
-                sum += map[i][j];
-            }
-        }
-
-        result = Math.max(sum, result);
-    }
+	static int T, N, M;
+	static int[][] map, smap;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		 
+		T = Integer.parseInt(st.nextToken());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			map = new int[N+1][N+1];
+			smap = new int[N+1][N+1];
+			for (int i = 1; i <= N; i ++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 1; j <= N; j++){
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			sb.append("#").append(test_case).append(" ").append(cal()).append("\n");		
+			
+		}
+		System.out.println(sb);
+	}
+	
+	private static int cal() {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				smap[i][j] = smap[i-1][j] + smap[i][j-1] - smap[i-1][j-1] + map[i][j];
+			}
+		}
+		
+		int[][] temp = new int[N+1][N+1];
+		int max = Integer.MIN_VALUE;
+		for (int x = M; x <= N; x++) {
+			for (int y = M; y <= N; y++) {
+				temp[x][y] = smap[x][y] - smap[x-M][y] - smap[x][y-M] + smap[x-M][y-M];
+				
+				max = Math.max(temp[x][y], max);
+			}
+		} 
+		return max;
+	}
 }
