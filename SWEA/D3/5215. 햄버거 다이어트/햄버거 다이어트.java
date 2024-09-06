3670292fe1c0145479c1d08c12658dcae36d7308
jaewonwi <80496853+jaewonwi@users.noreply.github.com>
2024-09-07 01:16:05 +0900

    [D3] Title: 햄버거 다이어트, Time: 828 ms, Memory: 21,152 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,61 +1,74 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int T, N, L, ans;
-	static int[] taste, kal;
-	static boolean[] select;
-	
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc <= T; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());	//재료의 수
-			L = Integer.parseInt(st.nextToken());	//제한 칼로리
-			
-			taste = new int[N];
-			kal = new int[N];
-			select = new boolean[N];
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				taste[i] = Integer.parseInt(st.nextToken());
-				kal[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			ans = Integer.MIN_VALUE;
-			subset(0);
-			
-			sb.append("#").append(tc).append(" ").append(ans).append("\n");
-		}
-			
-		System.out.println(sb);
-	}
-	
-	static void subset(int srcIdx) {
-		if (srcIdx == N) {	// 기저조건
-			int sumkal = 0;
-			int sumscore = 0;
-			for (int i = 0; i < N; i++) {
-				if (select[i]) {
-					sumkal += kal[i];
-					sumscore += taste[i];
-				}
-			}
-			if (sumkal <= L)
-				ans = Math.max(ans, sumscore);
-			
-			return;
-		}
-		
-		select[srcIdx] = true;
-		subset(srcIdx + 1);
-		select[srcIdx] = false;
-		subset(srcIdx + 1);
-		
-	}
+    static int TC, N, L, score;
+    static boolean[] select;
+    static List<Source> src;
+
+    static class Source {
+        int taste, kcal;
+        public Source(int t, int k){
+            this.taste = t;
+            this.kcal = k;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
+
+        TC = Integer.parseInt(st.nextToken());
+        for (int tc = 1; tc <= TC; tc++){
+            st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+
+            src = new ArrayList<>();
+            select = new boolean[N];
+            for (int i = 0; i < N; i++){
+                st = new StringTokenizer(br.readLine());
+                int t = Integer.parseInt(st.nextToken());
+                int k = Integer.parseInt(st.nextToken());
+                src.add(new Source(t, k));
+            }
+
+            score = Integer.MIN_VALUE;
+            subset(0);
+
+            sb.append("#").append(tc).append(" ").append(score).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+
+    static void subset(int idx){
+        if (idx == N){
+            // 선호도 계산 -> 갱신
+            int sumK = 0;
+            int sumT = 0;
+            for (int i = 0; i < N; i++){
+                if (select[i]){
+                    sumT += src.get(i).taste;
+                    sumK += src.get(i).kcal;
+                }
+            }
+
+            if (sumK <= L){
+                score = Math.max(score,sumT);
+            }
+
+            return;
+        }
+
+        select[idx] = true;
+        subset(idx + 1);
+        select[idx] = false;
+        subset(idx + 1);
+    }
 }
