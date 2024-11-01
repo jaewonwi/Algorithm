9ff73191d4e95f25bfffb4c4e1a9b195142b8177
위재원 <80496853+jaewonwi@users.noreply.github.com>
2024-11-01 11:57:20 +0900

    [Silver III] Title: 피보나치 함수, Time: 128 ms, Memory: 15980 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception{
+        int T = Integer.parseInt(br.readLine());
+        int[][] f = new int[41][2];
+        f[0][0] = 1;
+        f[1][1] = 1;
+        f[2][0] = 1;
+        f[2][1] = 1;
+        for (int i = 2; i < 41; i++){
+            f[i][0] = f[i-1][0] + f[i-2][0];
+            f[i][1] = f[i-1][1] + f[i-2][1];
+        }
+        for (int tc = 1; tc <= T; tc++){
+            int N = Integer.parseInt(br.readLine());
+
+            System.out.println(f[N][0] +" "+ f[N][1]);
+        }
+    }
+}
