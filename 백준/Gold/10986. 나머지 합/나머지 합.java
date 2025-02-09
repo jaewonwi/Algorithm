d97daa8791415b4d486809fdd5dec3f2836f3579
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-02-09 19:53:34 +0900

    [Gold III] Title: 나머지 합, Time: 460 ms, Memory: 120696 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -11,33 +11,31 @@ public class Main {
 
 	// ** (A+B) % C = ((A % C) + (B % C)) % C 를 이용
 	// 즉, 어떤 수의 나머지가 같으면, 그 두 수를 뺐을때 나머지는 0이 된다 = 나누어 떨어진다는 뜻
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 		st = new StringTokenizer(br.readLine().trim());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-		long[] S = new long[N];	// 합 배열
-		st = new StringTokenizer(br.readLine().trim());
-		S[0] = Integer.parseInt(st.nextToken());
-		for (int i = 1; i < N; i++) {    // 입력받으면서 누적합 배열 구하기
-			S[i] = S[i-1] + Integer.parseInt(st.nextToken());
-		}
+		long[] S = new long[N+1];    // 합 배열
+		long[] C = new long[M];
 
-		long[] C = new long[M]; // 나머지 카운트 배열
-		long cnt = 0;
-		for (int i = 0; i < N; i++){
+		long ans = 0;
+		st = new StringTokenizer(br.readLine().trim());
+		for (int i = 1; i <= N; i++){	// 입력받으면서 누적합 배열 구하기
+			S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+
 			int remain = (int) (S[i] % M);
-			if (remain == 0) cnt++;		// 애초에 나머지가 0인 것의 수
+			if (remain == 0) ans++;		// 애초에 나머지가 0인 것의 수
 			C[remain]++;
 		}
 
 		// 나머지가 같은 것 중 2개씩 뽑는 경우 * 나머지마다 수행
 		for (int i = 0; i < M; i++){
 			if (C[i] > 1) {
-				cnt += C[i] * (C[i]-1) / 2;
+				ans += C[i] * (C[i]-1) / 2;
 			}
 		}
 
-		System.out.println(cnt);
+		System.out.println(ans);
     }
 }
