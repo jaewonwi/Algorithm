0f15cae2dbac6c8fc0e95e7d89904b925f49023e
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-06-05 19:07:13 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.10 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < my_string.length(); i++){
+            set.add(my_string.substring(i));
+        }
+        return set.contains(is_suffix) ? 1 : 0;
+    }
+}
