cb1471ebd9f7b59866951b8f806566106c97fd72
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-07-02 13:59:43 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -2,79 +2,49 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// BOJ 1244. 스위치 켜고 끄기
 public class Main {
-    static int N, student;
-    static int[] switches;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
+		int n = Integer.parseInt(br.readLine());
+		boolean[] switches = new boolean[n+1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++){
+			switches[i] = Integer.parseInt(st.nextToken()) == 1 ? true : false;
+		}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine().trim());
-        StringBuilder sb = new StringBuilder();
+		int students = Integer.parseInt(br.readLine());
+		for (int i = 0; i < students; i++){
+			st = new StringTokenizer(br.readLine());
+			int gender = Integer.parseInt(st.nextToken());	// 1: 남, 2: 여
+			int number = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
+			if (gender == 1){
+				for (int j = number; j <= n; j += number){
+					switches[j] = !switches[j];
+				}
+			} else {
+				switches[number] = !switches[number];
+				for (int j = 1; j <= n; j++){
+					int left = number - j;
+					int right = number + j;
+					if (left <= 0 || right > n) break;
 
-        switches = new int[N+1];
+					if (switches[left] == switches[right]){
+						switches[left] = switches[right] = !switches[right];
+					} else
+						break;
+				}
+			}
+		}
 
-        st = new StringTokenizer(br.readLine().trim());
-        for (int i = 1; i <= N; i++){
-            switches[i] = Integer.parseInt(st.nextToken());
-        }
+		for (int i = 1; i <= n; i++){
+			sb.append(switches[i] ? 1:0).append(" ");
+			if (i % 20 == 0)
+				sb.append("\n");
+		}
 
-        student = Integer.parseInt(br.readLine().trim());
-        for (int i = 0; i < student; i++){
-            st = new StringTokenizer(br.readLine().trim());
-
-            int gender = Integer.parseInt(st.nextToken());
-            int num = Integer.parseInt(st.nextToken());
-
-            if (gender == 1) {
-                // 남자 로직
-                boy(num);
-            } else {
-                // 여자 로직
-                girl(num);
-            }
-        }
-
-        // 한 줄에 20개씩 스위치 출력하기
-        for (int i = 1; i <= N; i ++){
-            sb.append(switches[i]).append(" ");
-
-            if (i % 20 == 0){
-                sb.append("\n");
-            }
-        }
-
-        System.out.println(sb.toString());
-
-    }
-
-    static void boy(int n) {
-        for (int i = n; i <= N; i += n){
-            change(i);
-        }
-    }
-
-    static void girl(int n) {
-        change(n);
-
-        int idx = 1;
-        while (n - idx >= 1 && n + idx <= N) {
-            if (switches[n - idx] == switches[n + idx]){
-                change(n - idx);
-                change(n + idx);
-                idx++;
-            } else
-                break;
-
-        }
-    }
-
-    static void change(int n) {
-        if (switches[n] == 1)
-            switches[n] = 0;
-        else
-            switches[n] = 1;
-    }
+		System.out.println(sb);
+	}
 }
