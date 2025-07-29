e09b5a0d062ce118eb41cf623fffde69ec312662
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-07-29 19:50:16 +0900

    [Gold V] Title: 탑, Time: 620 ms, Memory: 86008 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,37 +1,25 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-// N 개의 높이가 서로다른 탑을 왼->오로 세운다.
-// 탑의 꼭대기에서 수평 왼쪽으로 레이저 발사. 탑의 기둥에 레이저 신호 수신
-// 가장 먼저 만나는 단 하나의 탑에서만 수신 가능
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
     static int N;
-    static int[] tower, answer;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine().trim());
-        StringBuffer sb = new StringBuffer();
-
-        N = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
+    static int[] tower, recieve;
+    public static void main(String[] args) throws Exception{
+        // 오 -> 왼 레이저 발사
+        // 레이저 수신: 가장 먼저 만나는 하나의 탑에서만 수신 가능
+        N = Integer.parseInt(br.readLine());
         tower = new int[N+1];
-        answer = new int[N+1];
+        recieve = new int[N+1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++){
             tower[i] = Integer.parseInt(st.nextToken());
         }
 
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = N; i >= 1; i--){
-            if (stack.isEmpty()) {
-                stack.push(i);
-                continue;
-            }
-
-            // 스택에서 꺼낸 애랑 현재 보고 있는 애 비교
             boolean flag = true;
             while (flag) {
                 if (stack.isEmpty()) {
@@ -42,23 +30,22 @@ public class Main {
 
                 int prev = tower[stack.peek()];
                 int cur = tower[i];
-                if (prev < cur) {   // 꺼내기
-                    answer[stack.pop()] = i;
-                } else if (prev > cur) {    // 넣기
+                if (prev < cur){
+                    recieve[stack.pop()] = i;
+                } else {
                     stack.push(i);
                     flag = false;
                 }
             }
-
-
         }
 
-        while (!stack.isEmpty()) {
-            answer[stack.pop()] = 0;
+        while (!stack.isEmpty()){
+            recieve[stack.pop()] = 0;
         }
 
-        for (int i = 1; i <= N; i++) sb.append(answer[i]).append(" ");
-
-        System.out.println(sb);
+        for (int i = 1; i <= N; i++){
+            sb.append(recieve[i]).append(" ");
+        }
+        System.out.print(sb);
     }
 }
