cc7ae64979cf749a2ad33b25e9d9b44ec17cf48e
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-09-18 15:18:43 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,7 +1,3 @@
-select ANIMAL_TYPE, 
-    case
-        when NAME is null then "No name"
-        else NAME
-    end as NAME, SEX_UPON_INTAKE
-from ANIMAL_INS
-order by ANIMAL_ID
+select ANIMAL_TYPE, IFNULL(NAME, "No name") as NAME, SEX_UPON_INTAKE
+  from ANIMAL_INS
+ order by ANIMAL_ID
