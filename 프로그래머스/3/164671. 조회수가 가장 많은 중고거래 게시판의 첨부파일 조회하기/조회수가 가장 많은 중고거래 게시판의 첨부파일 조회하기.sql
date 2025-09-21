3dfdb74492340046dbfc4b4384c898b6c6521829
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-09-21 09:14:57 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,4 @@
+select CONCAT('/home/grep/src/', b.board_id,'/', f.file_id, f.file_name, f.file_ext) as FILE_PATH
+  from USED_GOODS_BOARD b join USED_GOODS_FILE f on b.board_id = f.board_id
+ where b.views = (select max(views) from USED_GOODS_BOARD)
+ORDER BY F.FILE_ID DESC
