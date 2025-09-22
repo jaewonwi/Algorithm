b682f2aa07e6efbe5ac77d444496d5e418696a66
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-09-22 19:56:54 +0900

    [Gold V] Title: ABCDE, Time: 212 ms, Memory: 15648 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -4,62 +4,53 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-
-    static int N, M, a, b, ans;
-//    static boolean[][] matrix;
-    static List<List<Integer>> adjList = new ArrayList<>();
-    static boolean[] visit;
-
-    // A-B-C-D-E 순서로 친구인지 확인 (01234)
     public static void main(String[] args) throws Exception{
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+    	st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-//        matrix = new boolean[N][N];
-        for (int i = 0; i < N; i++){
-            adjList.add(new ArrayList<>());
-        }
+		List<List<Integer>> friends = new ArrayList<>();
+		for (int i = 0; i < N; i++)
+			friends.add(new ArrayList<>());
 
-        for (int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < M; i++){
+			st= new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
 
-//            matrix[a][b] = matrix[b][a] = true;
-            adjList.get(a).add(b);
-            adjList.get(b).add(a);
-        }
+			friends.get(A).add(B);
+			friends.get(B).add(A);
+		}
 
-        visit = new boolean[N];
-        for (int i = 0; i < N; i++){
-            visit[i] = true;
-            if (dfs(i, 1)) {
-                ans = 1;
-                break;
-            }
-            visit[i] = false;
-        }
+		int ans = 0;
+		boolean[] visit = new boolean[N];
+		for (int i = 0; i < N; i++){
+			visit[i] = true;
+ 			if (dfs(i,1, friends, visit)){
+				ans = 1;
+				break;
+			}
+			 visit[i] = false;
+		}
 
-        System.out.println(ans);
+		System.out.println(ans);
     }
 
-    static boolean dfs(int me, int cnt){
-        if (cnt >= 5){
-            return true;
-        }
+	static boolean dfs(int me, int cnt, List<List<Integer>> friends, boolean[] visit){
+		if (cnt >= 5){
+			return true;
+		}
 
-        List<Integer> myFriends = adjList.get(me);
-        for (int i = 0; i < myFriends.size(); i++){
-            int friend = myFriends.get(i);
-            if (visit[friend]) continue;
+		List<Integer> myFriend = friends.get(me);
+		for (int i = 0; i < myFriend.size(); i++){
+			int friend = myFriend.get(i);
+			if (visit[friend]) continue;
 
-            visit[friend] = true;
-            if (dfs(friend, cnt+1)) return true;
-            visit[friend] = false;
-        }
+			visit[friend] = true;
+			if (dfs(friend, cnt+1, friends, visit)) return true;
+			visit[friend] = false;
+		}
 
-        return false;
-    }
+		return false;
+	}
 }
