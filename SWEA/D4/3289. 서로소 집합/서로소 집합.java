3973acf9a98f6c95b639bccbceb026f9ba003353
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-10-21 19:45:52 +0900

    [D4] Title: 서로소 집합, Time: 346 ms, Memory: 104,480 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -5,56 +5,55 @@ public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-
-    static int T, n, m;
-    static int[] parent;
-
+    static int T, n, m, o, a, b;
+	static int[] parent;
     public static void main(String[] args) throws Exception{
-        T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++){
-            sb.append("#").append(tc).append(" ");
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
+    	T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++){
+			sb.append("#").append(t).append(" ");
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
 
-            make(n);
+			makeSet(n);
+			for (int i = 0; i < m; i++){
+				st = new StringTokenizer(br.readLine());
+				o = Integer.parseInt(st.nextToken());
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < m; i++){
-                st = new StringTokenizer(br.readLine());
-                int cal = Integer.parseInt(st.nextToken());
 
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
+				if (o == 0){
+					unionSet(a,b);
+				} else {
+					int pa = findSet(a);
+					int pb = findSet(b);
 
-                if (cal == 0){
-                    union(a, b);
-                } else if (cal == 1) {
-                    if (find(a) == find(b)) sb.append(1);
-                    else sb.append(0);
-                }
-            }
-            sb.append("\n");
-        }
+					if (pa == pb) sb.append(1);
+					else sb.append(0);
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
 
-        System.out.println(sb);
-    }
+	static void makeSet(int n){
+		parent = new int[n+1];
+		for (int i = 0; i <= n; i++){
+			parent[i] = i;
+		}
+	}
 
-    static boolean union(int a, int b){
-        if (find(a) == find(b)) return false;
+	static boolean unionSet(int a, int b){
+		if (findSet(a) == findSet(b)) return false;
 
-        parent[find(b)] = find(a);
-        return true;
-    }
+		parent[findSet(b)] = a;
+		return true;
+	}
 
-    static int find(int x){
-        if (parent[x] == x) return x;
-        else return parent[x] = find(parent[x]);    // ** path compression!
-    }
-
-    static void make(int x){
-        parent = new int[x+1];
-        for (int i = 0; i <= x; i++){
-            parent[i] = i;
-        }
-    }
+	static int findSet(int a){
+		if (parent[a] == a) return a;
+		return parent[a] = findSet(parent[a]);
+	}
 }
