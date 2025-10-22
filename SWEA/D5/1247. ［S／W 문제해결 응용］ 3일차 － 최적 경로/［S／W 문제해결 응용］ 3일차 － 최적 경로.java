0300c456ec570cfa7473f3798a30ddeb3f7c789a
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-10-22 19:47:28 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 1,514 ms, Memory: 26,876 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,89 +1,82 @@
 import java.io.*;
 import java.util.*;
 
+// swea_1247
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-    static Pos home, company;
-    static List<Pos> customer;
-    static int T, N, x, y, answer;
-    static int[] tgt;
-    static boolean[] select;
+	static int T, N, ans;
+	static Pos home, company;
+	static List<Pos> customer = new ArrayList<>();
+	static int[] target;
+	static boolean[] select;
 
-
-    // 회사, 집 좌표 + 2~10명의 고객 좌표 => 회사 -> 모든 고객 -> 집 경로 중 이동거리가 가장 짧은 경로 찾기
     public static void main(String[] args) throws Exception{
-        T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++){
-            sb.append("#").append(tc).append(" ");
+		T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++){
+			N = Integer.parseInt(br.readLine().trim());
+			st = new StringTokenizer(br.readLine().trim());
+			company = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			home = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
-            N = Integer.parseInt(br.readLine());
-            tgt = new int[N];
-            select = new boolean[N];
+			customer.clear();
+			ans = Integer.MAX_VALUE;
 
-            st = new StringTokenizer(br.readLine().trim());
-            company = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            home = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            customer = new LinkedList<>();
-            for (int i = 0; i < N; i++){
-                x = Integer.parseInt(st.nextToken());
-                y = Integer.parseInt(st.nextToken());
-                customer.add(new Pos(x, y));
-            }
+			for (int i = 0; i < N; i++){
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				customer.add(new Pos(x,y));
+			}
 
-            answer = Integer.MAX_VALUE;
-            perm(0, 0);
+			// 풀이
+			target = new int[N];
+			select = new boolean[N];
+			perm(0);
 
-            sb.append(answer).append("\n");
-        }
-
-        System.out.println(sb);
+			sb.append("#").append(t).append(" ").append(ans).append("\n");
+		}
+		System.out.println(sb);
     }
 
-    static void perm(int tgtIdx, int sum){
-        if (tgtIdx == N){
-            // complete code
-//            int sum = distance(company, customer.get(tgt[0]));
-//            for (int i = 0; i < N-1; i++){
-//                sum += distance(customer.get(tgt[i]), customer.get(tgt[i+1]));
-//            }
+	static void cal(){
+		int sum = 0;
+		Pos cur = company;
+		for (int i = 0; i < N; i++){
+			Pos next = customer.get(target[i]);
+			sum += getDistance(cur, next);
+			cur = next;
+		}
+		sum += getDistance(cur, home);
 
-            sum += distance(customer.get(tgt[N-1]), home);
-            answer = Math.min(answer, sum);
-            return;
-        }
+		ans = Math.min(sum, ans);
+	}
 
-        for (int i = 0; i < N; i++){
-            if (select[i]) continue;
+	static void perm(int cnt){
+		if (cnt == N){
+			cal();
+			return;
+		}
 
-            tgt[tgtIdx] = i;
+		for (int i = 0; i < N; i++){
+			if (select[i]) continue;
 
-            int d = 0;
-            if (tgtIdx == 0){
-                d = distance(company, customer.get(tgt[0]));
-            } else {
-                d = distance(customer.get(tgt[tgtIdx-1]), customer.get(tgt[tgtIdx]));
-            }
+			target[cnt] = i;
+			select[i] = true;
+			perm(cnt+1);
+			select[i] = false;
+		}
+	}
 
-            if (sum + d < answer){
-                select[i] = true;
-                perm(tgtIdx+1,sum + d);
-                select[i] = false;
-            }
-        }
-    }
+	static int getDistance(Pos a, Pos b){
+		return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+	}
 
-    static int distance(Pos p1, Pos p2){   // distance = |x1-x2| + |y1-y2|
-        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
-    }
+	static class Pos{
+		int x, y;
+		public Pos(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
-
-class Pos{
-    int x, y;
-    Pos(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-
