4b88b611899a38b448c872e94368a6c0b0207a11
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-11-05 14:09:50 +0900

    [Gold III] Title: 게리맨더링, Time: 120 ms, Memory: 15332 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -4,105 +4,91 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-
-    static int N, ans;   // 1~N번 구역 -> 2개의 선거구로 나누어야 하며, 한 선거구는 모두 연결되어있어야 함
+    static int N, ans = Integer.MAX_VALUE;
     static int[] people;
-    static boolean[] select, visit;
-    static List<List<Integer>> list = new ArrayList<>();
-
+    static boolean[] select, visit;     // select: 구역 선택 여부, visit: 방문 체크용
+    static List<List<Integer>> adjList = new ArrayList<>();
     public static void main(String[] args) throws Exception{
-        N = Integer.parseInt(br.readLine());    // 구역의 수
-
-        // 인구 수 입력
+        N = Integer.parseInt(br.readLine());
         people = new int[N];
+        select = new boolean[N];
+        visit = new boolean[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++){
             people[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 인접 구역 정보
         for (int i = 0; i < N; i++){
-            list.add(new ArrayList<>());
+            adjList.add(new ArrayList<>());
             st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            for(int n = 0; n < num; n++){
-                int next = Integer.parseInt(st.nextToken()) - 1;
-                list.get(i).add(next);
+            int cnt = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < cnt; j++){
+                adjList.get(i).add(Integer.parseInt(st.nextToken()) - 1);
             }
         }
 
-        ans = Integer.MAX_VALUE;
-        select = new boolean[N];
-        visit = new boolean[N];
-
+        // 조합 - 두 선거구 나누기
         subset(0);
-
         System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
     }
 
-    static int calDiff(){
-        int sumA = 0;
-        int sumB = 0;
+    static void subset(int idx){
+        if (idx == N){;
+            calculate();
+            return;
+        }
 
+        select[idx] = true;
+        subset(idx+1);
+        select[idx] = false;
+        subset(idx+1);
+    }
+
+    static void calculate(){
+        // A, B 구역이 각각 연결되어있는지 체크
+        Arrays.fill(visit, false);
+        int cntA = 0, cntB = 0;                    // a,b 구역 탐색 시작점
+        for (int i = 0; i < N; i++){
+            if (select[i]) {
+                cntA = connect(i, true);
+                break;
+            }
+        }
+
+        for (int i = 0; i < N; i++){
+            if (!select[i]) {
+                cntB = connect(i, false);
+                break;
+            }
+        }
+
+        if (cntA+cntB != N) return;
+
+        // 각 구역의 인구수 차이 체크 후 갱신
+        int sumA = 0, sumB = 0;
         for (int i = 0; i < N; i++){
             if (select[i]) sumA += people[i];
             else sumB += people[i];
         }
 
-        return Math.abs(sumA-sumB);
+        ans = Math.min(ans, Math.abs(sumA - sumB));
     }
 
-    static int bfs(int start, boolean flag){  // 연결 체크
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(start);
-        visit[start] = true;
-
+    static int connect(int start, boolean flag){
         int cnt = 0;
-        while (!queue.isEmpty()){
-            int cur = queue.poll();
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        visit[start] = true;
+        while (!q.isEmpty()){
+            int cur = q.poll();
             cnt++;
+            for (int next : adjList.get(cur)){
+                if (!select[next] == flag || visit[next]) continue;
 
-            for (int i = 0; i < list.get(cur).size(); i++){
-                int next = list.get(cur).get(i);
-                if (select[next] == flag && !visit[next]){
-                    visit[next] = true;
-                    queue.add(next);
-                }
+                visit[next] = true;
+                q.offer(next);
             }
         }
-
         return cnt;
-    }
-
-    static void subset(int srcIdx){
-        if (srcIdx == N){
-            Arrays.fill(visit, false);
-
-            int connA = 0, connB = 0;
-            for (int i = 0; i < N; i++){
-                if (select[i]){
-                    connA = bfs(i, true);
-                    break;
-                }
-            }
-            for (int i = 0; i < N; i++){
-                if (!select[i]){
-                    connB = bfs(i, false);
-                    break;
-                }
-            }
-
-            if (connA + connB == N){    // 다 연결되어있다면
-                ans = Math.min(ans, calDiff());     // 차이가 최소가 되도록 갱신
-            }
-
-            return;
-        }
-
-        select[srcIdx] = true;
-        subset(srcIdx+1);
-        select[srcIdx] = false;
-        subset(srcIdx+1);
     }
 }
