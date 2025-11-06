c8ac9501b325274585b240d1167ce49f0785c387
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-11-06 17:07:16 +0900

    [Gold IV] Title: 최단경로, Time: 692 ms, Memory: 109684 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,68 +1,61 @@
 import java.io.*;
 import java.util.*;
 
-// Dijkstra
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
     static int V, E, K;
-    static List<List<Edge>> adjList = new ArrayList<>();
-    static int[] weight;    // 다익스트라 자료구조
-    static boolean[] visit;
-
-    static PriorityQueue<Edge> pq = new PriorityQueue<>((e1, e2) -> e1.w - e2.w);
-    static final int INF = Integer.MAX_VALUE;
-
-
-
+    static List<List<Edge>> list = new ArrayList<>();
+    static int[] w;
     public static void main(String[] args) throws Exception{
         st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(br.readLine());
+        V = Integer.parseInt(st.nextToken());   // 정점 수 <= 20_000 (번호:1~V)
+        E = Integer.parseInt(st.nextToken());   // 간선 수 <= 300_000
+        K = Integer.parseInt(br.readLine());    // 시작 정점 번호
 
-        weight = new int[V+1];
-        visit = new boolean[V+1];
-        for (int i = 0; i <= V; i++){
-            adjList.add(new ArrayList<>());
+        w = new int[V+1];   // 다익스트라 결과 저장 배열
+        Arrays.fill(w, Integer.MAX_VALUE);
+
+        for (int i = 0; i <= V; i++){   // 인접 리스트
+            list.add(new ArrayList<>());
         }
-        Arrays.fill(weight, INF);
 
-        for (int i = 0; i < E; i++){
+        for (int i = 0; i < E; i++){    // 입력
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-
-            adjList.get(u).add(new Edge(v, w));
+            list.get(u).add(new Edge(v, w));
         }
 
         dijkstra();
 
         for (int i = 1; i <= V; i++){
-            System.out.println(weight[i] == INF ? "INF" : weight[i]);
+            sb.append(w[i] == Integer.MAX_VALUE ? "INF" : w[i]).append("\n");
         }
+        System.out.println(sb);
     }
 
     static void dijkstra(){
-        weight[K] = 0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>((e1, e2) -> e1.w - e2.w);
+        boolean[] visit = new boolean[V+1];
         pq.offer(new Edge(K, 0));
+        w[K] = 0;
 
         while (!pq.isEmpty()){
-            Edge e = pq.poll();     // 최소비용 간선
-            if (visit[e.v]) continue;   // 이미 그래프에 포함되어있는 간선이라면 패스! -> 간선의 가중치가 양수이기 때문에 어떻게 해도 경로 길어짐
+            Edge cur = pq.poll();
+            if (visit[cur.v]) continue; // 이미 그래프에 포함 된 간선이라면 패스 (가중치가 양수이기 때문에 다른 곳을 더 거치면 가중치가 늘어난다)
 
-            visit[e.v] = true;
+            visit[cur.v] = true;
 
-            for (Edge ne : adjList.get(e.v)){   // e에서 갈수 있는 정점들 ne
-                // ne.v가 갈수있는 정점이 아직 방문하지 않았으면서 동시에 가중치를 줄여주는 정점일 경우
-                if (ne.w + weight[e.v] < weight[ne.v]){
-                    weight[ne.v] = ne.w + weight[e.v];
+            for (Edge ne : list.get(cur.v)){
+                if (ne.w + w[cur.v] < w[ne.v]){
+                    w[ne.v] = ne.w + w[cur.v];  //'출발지-현재 정점'까지 가중치 + '현재 정점-다음 정점'까지 가중치
 
-                    pq.offer(new Edge(ne.v, weight[ne.v]));
+                    pq.offer(new Edge(ne.v, w[ne.v]));
                 }
-                    weight[e.v] = e.w;
+                w[cur.v] = cur.w;
             }
         }
     }
@@ -74,5 +67,4 @@ public class Main {
             this.w = w;
         }
     }
-
 }
