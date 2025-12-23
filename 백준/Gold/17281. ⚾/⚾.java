dcb5f3d8090225b5068d6b7cc9928cca3928c393
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-12-24 00:17:04 +0900

    [Gold IV] Title: ⚾, Time: 544 ms, Memory: 18660 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -1,99 +1,99 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static int N, sum, max = 0;
-    static int[][] inning;
-    static int[] tgt;
-    static boolean[] visit, field;
-    // 9명, 하나의 이닝 = 공격 & 수비, N번의 이닝, 한 이닝 -> 3아웃 -> 이닝 종료 -> 공수 변경
-    // 시작 전 타순을 정하고(9 -> 1번 타자), 순서는 계속 유지됨(이전 이닝의 마지막 타자 다음 순서부터 다음 이닝 시작)
-    // 1-2-3-홈에 도착하면 1점, 이닝 시작 시 주자는 없다.
-    // 공을 쳐서 얻을 수 있는 결과: (모든 주자가) 안타(1루), 2루타(2루), 3루타(3루), 홈런(홈까지 진루), 아웃(진루X, 아웃+1)
-    // 1번선수 = 4번타자. 가장 많은 득점을 하는 타순은?
+    static int N, ans = 0;
+	static int[][] inning;
+	static int[] tgt;
+	static boolean[] visit, ground;
     public static void main(String[] args) throws Exception{
-        N = Integer.parseInt(br.readLine());
-        inning = new int[N][9];
-        for (int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 9; j++){
-                inning[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		// N 이닝, 3아웃 -> 이닝 종료 & 공수 교대
+		// 타순 변경 X, 4>...>8>9>1>2>..., 이닝 변경 후 타순 유지
+		// 이닝 시작 시 주자 X
+		// 안타(모두 1루 진루), 2루타, 3루타, 홈런(모두 홈 진루), 아웃(진루X,아웃+)
+		// 입력
+		N = Integer.parseInt(br.readLine());
+		inning = new int[N][9];
+		for (int n = 0; n < N; n++){
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 9; i++){
+				inning[n][i] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        visit = new boolean[9];
-        tgt = new int[9];
-        field = new boolean[4];   // home 1, 2, 3루 상태 표시
-        visit[0] = true;    // 1번 타자 4번째 순서 고정
-        tgt[3] = 0;
+		tgt = new int[9];
+		visit = new boolean[9];
+		ground = new boolean[4];	// 홈, 1, 2, 3루
 
-        perm(0);
+		tgt[3] = 0;	// 1번 타자는 4번째 고정
 
-        System.out.println(max);
+		perm(0);
+
+		System.out.println(ans);
     }
 
-    static void perm(int tgtIdx){
-        if (tgtIdx == 9) {
-            simulate();
-            return;
-        }
+	static void perm(int cnt){
+		if (cnt == tgt.length){
+			// 순서가 완성되면
+			start();
+			return;
+		}
 
-        for (int i = 1; i < 9; i++){
-            if (visit[i]) continue;
+		for (int i = 1; i < 9; i++){
+			if (visit[i]) continue;
 
-            visit[i] = true;
-            if (tgtIdx == 3) tgtIdx++;
-            tgt[tgtIdx] = i;
-            perm(tgtIdx+1);
-            visit[i] = false;
-        }
-    }
+			visit[i] = true;
+			if (cnt == 3) cnt++;	// ** tgt[3]은 이미 0번 선수로 고정되어 있으니 다음 자리로 바로 넘기면 됨
+			tgt[cnt] = i;
+			perm(cnt+1);
+			visit[i] = false;
+		}
+	}
 
-    static void simulate(){
-        sum = 0;
-        int idx = 0;
-        for (int i = 0; i < N; i++){
-            Arrays.fill(field, false);  // 필드 초기화
-            int out = 0;
-            while (out < 3){
-                int hitter = tgt[(idx++) % 9];   // 현재 타자
-                int hit = inning[i][hitter];     // 타자가 이번 게임에서 얻은 결과
-                field[0] = true;
+	static void start(){
+		int score = 0;
+		int idx = 0;	// order[]의 몇번째 선수인지;
+		for (int i = 0; i < N; i++){
+			// 이닝 시작 - 아웃 count 0, 경기장에 아무도 없음
+			int out = 0;
+			Arrays.fill(ground, false);
 
-                if (hit == 0){
-                    field[0] = false;
-                    out++;
-                } else {
-                    sum += getPoint(hit);
-                }
+			while (out < 3){	// 아웃 3개 - 다음 이닝
+				int player = tgt[idx];			// 현재 타자
+				int hit = inning[i][player];	// 현재 타자가 이번 게임에서 얻은 결과
 
-                max = Math.max(sum, max);
-            }
-        }
+				ground[0] = true;	// 홈에 타자
+				if (hit == 0) {
+					ground[0] = false;	// 아웃이면 바로 나감
+					out++;
+				} else
+					score += getPoint(hit);	// 아웃이 아니면 점수 계산
 
-        max = Math.max(sum, max);
-    }
+				idx = (idx + 1) % 9;	// 다음 타자
+			}
+		}
 
-    static int getPoint(int hit){
-        int point = 0;
+		ans = Math.max(ans, score);
+	}
 
-        for (int i = 3; i >= 0; i--){
-            if (!field[i]) continue;
+	static int getPoint(int hit){
+		int point = 0;
+		for (int i = 3; i >= 0; i--){
+			if (!ground[i]) continue;
 
-            if (i+hit > 3) {
-                point++;
-                field[i] = false;
-            }
-            else {
-                field[i+hit] = true;
-                field[i] = false;
-            }
-        }
+			if (hit + i > 3){
+				point++;
+			} else {
+				ground[hit+i] = true;
+			}
+			ground[i] = false;
+		}
 
-        return point;
-    }
+		return point;
+	}
+
+
+
 }
