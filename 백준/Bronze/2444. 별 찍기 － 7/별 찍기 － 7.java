01f99818ad02ef6cd3614221c4822e1e1342dba4
위재원 <80496853+jaewonwi@users.noreply.github.com>
2026-01-20 18:40:19 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 108 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -12,12 +12,8 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 
 		for (int i = 1; i <= N; i++){
-			for (int j = 0; j < (N-i); j++){
-				sb.append(" ");
-			}
-			for (int j = 0; j < 2*i-1; j++){
-				sb.append("*");
-			}
+			sb.append(" ".repeat(N-i));
+			sb.append("*".repeat(2*i-1));
 			sb.append("\n");
 		}
 		for (int i = N-1; i >= 1; i--){
